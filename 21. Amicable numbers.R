941cd2c438920e34715426e35d48bebe772ebeca
Ronak Shah <shahronak47@yahoo.in>
2017-07-17 23:31:33 +0800

    Addded as a function

--- 21. Amicable numbers.R
+++ 21. Amicable numbers.R
@@ -1,5 +1,5 @@
 source("C:/Users/Ronak Shah/Google Drive/Git-Project-Euler/12.Get_Factors.R")
-n = 9999
+get_sum_of_amicable_number_under_n <- function(n) {
 amicable_numbers = numeric()
 for(i in 3:n) {
   #Excluding the number itself, as they are sorted so excluding the last one
@@ -8,7 +8,7 @@ for(i in 3:n) {
   opposite_divisors = head(getFactors2(opposite_number), -1)
   if (sum(opposite_divisors) == i & i != opposite_number & !(i %in% amicable_numbers)) {
     amicable_numbers =  append(amicable_numbers, c(i, opposite_number))
-    print(amicable_numbers)
   }
 }
-print(sum(amicable_numbers))
+return(sum(amicable_numbers))
+}
