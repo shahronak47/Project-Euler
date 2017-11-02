d531d2efdea160eaa0c9ddec9e0a32eda52b87b0
Ronak Shah <shahronak47@yahoo.in>
2017-11-02 23:53:24 +0800

    updated with system times for different OS

--- 39.Integer_right_trinagles.R
+++ 39.Integer_right_trinagles.R
@@ -14,9 +14,6 @@ get_max_solution_pythagorean_triplet_v1 <- function(limit) {
   return(which.max(table(sum_list[sum_list <= limit])))
 }
 
-system.time(get_max_solution_pythagorean_triplet_v1(1000))
-#user  system elapsed 
-#13.240   0.043  13.324 
 
 get_max_solution_pythagorean_triplet_v2 <- function(limit) {
   limit = 1000
@@ -34,9 +31,6 @@ get_max_solution_pythagorean_triplet_v2 <- function(limit) {
   return(which.max(table(sapply(all_list, sum))))
 }
 
-system.time(get_max_solution_pythagorean_triplet_v2(1000))
-#user  system elapsed 
-#14.483   0.059  14.591 
 
 get_max_solution_pythagorean_triplet_v3 <- function(limit) {
   limit = 1000
@@ -56,6 +50,29 @@ get_max_solution_pythagorean_triplet_v3 <- function(limit) {
   return(which.max(table(sapply(all_list, sum))))
 }
 
+#On mac
+
+system.time(get_max_solution_pythagorean_triplet_v1(1000))
+#user  system elapsed 
+#13.240   0.043  13.324 
+
+system.time(get_max_solution_pythagorean_triplet_v2(1000))
+#user  system elapsed 
+#14.483   0.059  14.591 
+
 system.time(get_max_solution_pythagorean_triplet_v3(1000))
 #user  system elapsed 
 #22.134   0.183  22.602 
+
+
+#On windows
+
+system.time(get_max_solution_pythagorean_triplet_v1(1000))
+#user  system elapsed 
+#239.67    0.11  243.28 
+system.time(get_max_solution_pythagorean_triplet_v2(1000))
+#user  system elapsed 
+#73.14    0.04   73.89 
+system.time(get_max_solution_pythagorean_triplet_v3(1000))
+#user  system elapsed 
+#95.28    0.02   96.14 
