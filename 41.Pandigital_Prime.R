fb9cc4c6bb2f94e926232585f33b196085891d44
Ronak Shah <shahronak47@yahoo.in>
2017-11-08 21:59:51 +0800

    Added system timings

--- 41.Pandigital_Prime.R
+++ 41.Pandigital_Prime.R
@@ -1,6 +1,6 @@
-source("/Users/ronakshah/Google Drive/Project Euler/3.Check_if_Prime.R")
-source("/Users/ronakshah/Google Drive/Project Euler/41.Get_Numbers_List.R")
-source("/Users/ronakshah/Google Drive/Project Euler/32.Is_Pandigital.R")
+source("C:\\Users\\Ronak Shah\\Google Drive\\Git-Project-Euler\\3.Check_if_Prime.R")
+source("C:\\Users\\Ronak Shah\\Google Drive\\Git-Project-Euler\\41.Get_Numbers_List.R")
+source("C:\\Users\\Ronak Shah\\Google Drive\\Git-Project-Euler\\32.Is_Pandigital.R")
 
 library(combinat)
 get_pandigital_prime <- function() {
@@ -10,9 +10,8 @@ get_pandigital_prime <- function() {
     all_numbers = sort(unlist(permn(1:i, function(x) as.numeric(paste0(x, collapse = "")))), decreasing = T)
     odd_numbers = get_odd_numbers_from_list(all_numbers)
     for (number in odd_numbers) {
-      print(number)
       if(is_prime(number)) {
-        cat("Highest pandigital prime is ", number)
+        cat("Highest pandigital prime is ", number, "\n")
         flag = 1
         break
       }
@@ -20,3 +19,9 @@ get_pandigital_prime <- function() {
     if(flag) break
   }
 }
+
+system.time(get_pandigital_prime())
+#Highest pandigital prime is  7652413 
+
+#user  system elapsed 
+#2.70    0.06    2.76
