18e2ebcb785732e68c2c2c92033b94ce9b1f83c9
Ronak Shah <shahronak47@yahoo.in>
2017-12-06 22:18:13 +0800

    added version 2 of problem 46

--- 41-50/46.Goldbachs.R
+++ 41-50/46.Goldbachs.R
@@ -24,7 +24,49 @@ get_answer_for_problem46_v1 <- function() {
           }
       }
       if(!flag)
-        cat("The amswer is : ", start_num)
+        cat("The amswer is : ", start_num, "\n")
     }
   }
 }
+
+system.time(get_answer_for_problem46_v1())
+
+#  user  system elapsed 
+# 193.71    1.00  197.95 
+
+
+get_answer_for_problem46_v2 <- function() {
+  start_num <- 33
+  flag = TRUE
+  while(flag) {
+    start_num = start_num + 2
+    if(is_prime(start_num))
+      next
+    else {
+      flag = FALSE
+      primes <- get_primes_under_n(start_num - 2)
+      square_number = 1
+      for (i in primes) {
+        for (square_number in seq(start_num)) {
+          current_sum = i + (2 * (square_number^2))
+          if (current_sum > start_num)
+            break
+          else if (current_sum == start_num) {
+            cat(start_num, "=", i, "+ 2 X", square_number, "^2\n")
+            flag = TRUE
+            break
+          }
+        }
+        if(flag)
+          break
+      }
+      if(!flag)
+        cat("The amswer is : ", start_num, "\n")
+    }
+  }
+}
+
+system.time(get_answer_for_problem46_v2())
+
+#user  system elapsed 
+#75.73    0.27   77.06 
