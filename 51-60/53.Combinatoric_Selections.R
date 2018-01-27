83d5216ad4dc276ba33f3da187e79c5b5f07b6d4
Ronak Shah <shahronak47@yahoo.in>
2018-01-27 23:32:50 +0800

    added as a function and time

--- 51-60/53.Combinatoric_Selections.R
+++ 51-60/53.Combinatoric_Selections.R
@@ -1,17 +1,21 @@
-counter = 0
-for(i in seq(1, 14)) {
-  for(j in seq(i)) {
-    if(ncol(combn(i, j)) > 100) {
-      mid = floor(i/2)
-      if (i %% 2 == 0) {
-        counter = counter + (2*(mid - j) + 1)
+get_combinatoric_selection <- function(max_number, limit) {
+  counter = 0
+  for(i in seq(23, max_number)) {
+    for(j in seq(i)) {
+      if(ncol(combn(i, j)) > limit) {
+        mid = floor(i/2)
+        if (i %% 2 == 0) {
+          counter = counter + (2*(mid - j) + 1)
+        }
+        else {
+          counter = counter + (2*(mid - j) + 2)
+        }
+        break
       }
-      else {
-        counter = counter + (2*(mid - j) + 2)
-      }
-      break
     }
   }
 }
 
-sapply(seq(18), function(x) sum(ncol(combn(18, x)) > 100))
+system.time(get_combinatoric_selection(100, 1000000))
+#user  system elapsed 
+#303.96    2.14  306.78 
