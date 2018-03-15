3b7ddbbf8deabdfca52faacc1c26da243e5b19a9
Ronak Shah <shahronak47@yahoo.in>
2018-03-15 23:30:58 +0800

    added output and system time

--- 51-60/55.Lychrel numbers.R
+++ 51-60/55.Lychrel numbers.R
@@ -1,15 +1,22 @@
 library(stringi)
 lychrel_num = numeric()
-for (i in seq(10000)) {
-  cat("Iteration: ", i, "\n")
-  num = i + as.numeric(stri_reverse(i))
-  count = 0
-  while(num != stri_reverse(num) & count < 50) {
-    num = num + as.numeric(stri_reverse(num))
-    cat(num)
-    count = count + 1
+lychrel_nums_under_n <- function(limit) {
+  for (i in seq(limit)) {
+    num = i + as.numeric(stri_reverse(i))
+    count = 0
+    while(num != stri_reverse(num) & count < 50) {
+      num = num + as.numeric(stri_reverse(num))
+      count = count + 1
+    }
+    if(count == 50)
+      lychrel_num = c(lychrel_num, i)
   }
-  if(count == 50)
-    lychrel_num = c(lychrel_num, i)
+  return(lychrel_num)
 }
 
+length(lychrel_nums_under_n(10000))
+#[1] 249
+
+system.time(lychrel_nums_under_n(10000))
+#user  system elapsed 
+#0.75    0.00    0.75 
